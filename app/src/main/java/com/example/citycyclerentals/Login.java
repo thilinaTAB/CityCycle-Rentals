9ddebd069799a9e4375a3ee8c748f59259f70684
thilinaAB <bowattatab@gmail..com>
2025-03-23 23:41:16 +0530

    Login and register pages UI complete 90%

--- app/src/main/java/com/example/citycyclerentals/Login.java
+++ app/src/main/java/com/example/citycyclerentals/Login.java
@@ -1,6 +1,12 @@
 package com.example.citycyclerentals;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +16,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    TextView txt_btnRegister;
+    EditText etxt_Email,etxt_Password;
+    Button btn_Login;
+    boolean valid = false;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,36 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txt_btnRegister = findViewById(R.id.TXT_btnRegister);
+        etxt_Email = findViewById(R.id.ETXT_Email);
+        etxt_Password = findViewById(R.id.ETXT_Password);
+        btn_Login = findViewById(R.id.BTN_Login);
+
+
+        txt_btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent moveReg =new Intent(getApplicationContext(), Register.class);
+                startActivity(moveReg);
+            }
+        });
+
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkField(etxt_Email);
+                checkField(etxt_Password);
+            }
+        });
+    }
+    public boolean checkField(EditText ex){
+        if (ex.getText().toString().isEmpty()){
+            ex.setError("Fill This");
+            valid = false;
+        }else{
+            valid = true;
+        }
+        return valid;
     }
 }
--- app/src/main/java/com/example/citycyclerentals/Register.java
+++ app/src/main/java/com/example/citycyclerentals/Register.java
@@ -1,6 +1,11 @@
 package com.example.citycyclerentals;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Register extends AppCompatActivity {
+
+    TextView txt_btnLogin;
+
+    EditText etxt_Name, etxt_Email, etxt_Password;
+
+    boolean valid = false;
+
+    Button btn_Register;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,41 @@ public class Register extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txt_btnLogin = findViewById(R.id.TXT_btnLogin);
+        etxt_Name = findViewById(R.id.ETXT_Name);
+        etxt_Email = findViewById(R.id.ETXT_Email);
+        etxt_Password = findViewById(R.id.ETXT_Password);
+        btn_Register = findViewById(R.id.BTN_Register);
+
+        txt_btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent moveLogin = new Intent(getApplicationContext(), Login.class);
+                startActivity(moveLogin);
+            }
+        });
+
+        btn_Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkField(etxt_Name);
+                checkField(etxt_Email);
+                checkField(etxt_Password);
+
+            }
+        });
+    }
+
+    public boolean checkField(EditText ex) {
+        if(ex.getText().toString().isEmpty()){
+            ex.setError("Required this field");
+            valid = false;
+        }else{
+            valid = true;
+        }
+
+
+        return valid;
     }
 }

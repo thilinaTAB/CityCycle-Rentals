1de41147a6872926ee2915f3259836d2782ffbf4
Thilina Bowatta <bowattatab@gmail.com>
2025-04-12 02:33:57 +0530

    software updated

--- app/src/main/java/com/example/citycyclerentals/UserDashboard.java
+++ app/src/main/java/com/example/citycyclerentals/UserDashboard.java
@@ -1,5 +1,7 @@
 package com.example.citycyclerentals;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
